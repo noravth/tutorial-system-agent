d880151474984d8d0dd8fd6703a837835ed7378b
Nora von Thenen <nora.von.thenen@sap.com>
2025-07-09 14:55:30 +0200

    add first version of basic agent to check tutorials

---
+++ tutorial-ai-agent/agent/genaihub_client.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+import variables
+
+ROOT_PATH_DIR = os.path.dirname(os.getcwd())
+AICORE_CONFIG_FILENAME = 'tutorial-system-agent/tutorial-ai-agent/.aicore-config.json'
+
+def set_environment_variables() -> None:
+    with open(os.path.join(ROOT_PATH_DIR, AICORE_CONFIG_FILENAME), 'r') as config_file:
+        config_data = json.load(config_file)
+
+    os.environ["AICORE_AUTH_URL"]=config_data["url"]+"/oauth/token"
+    os.environ["AICORE_CLIENT_ID"]=config_data["clientid"]
+    os.environ["AICORE_CLIENT_SECRET"]=config_data["clientsecret"]
+    os.environ["AICORE_BASE_URL"]=config_data["serviceurls"]["AI_API_URL"]
+
+    os.environ["AICORE_RESOURCE_GROUP"]=variables.RESOURCE_GROUP
---
+++ tutorial-ai-agent/agent/main.py
@@ -0,0 +1,16 @@
+
+from dataset_loader import MarkdownDatasetLoader
+from basic_agent import analyze_markdown_files
+
+# Example usage:
+#loader = MarkdownDatasetLoader()
+#loader.load_from_subfolder("appgyver-create-application")
+#loader.pretty_print()
+#loader.append_from_subfolder("cp-cap-java-app-studio")
+#loader.pretty_print()
+
+
+
+
+# Example usage:
+analyze_markdown_files("appgyver-create-application")
